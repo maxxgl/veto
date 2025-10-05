4e89ab48102f02ca8461993b1eb7604c151ca6c0
Maxx Lehmann <mglehman@mtu.edu>
2025-10-05 12:14:20 -0600

    get restaurants

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,61 +1,6 @@
 import type { Actions } from './$types';
 import { redirect, error } from '@sveltejs/kit';
-import { db, generateShortCode } from '$lib';
-
-interface Restaurant {
-	name: string;
-	lat: number;
-	lon: number;
-	cuisine?: string;
-}
-
-interface OverpassElement {
-	tags?: { name?: string; cuisine?: string };
-	lat?: number;
-	lon?: number;
-	center?: { lat: number; lon: number };
-}
-
-interface OverpassResponse {
-	elements: OverpassElement[];
-}
-
-async function getRestaurantsFromLocation(
-	latitude: number,
-	longitude: number,
-	radius = 2000
-): Promise<Restaurant[]> {
-	const query = `
-		[out:json];
-		(
-			node["amenity"="restaurant"](around:${radius},${latitude},${longitude});
-			way["amenity"="restaurant"](around:${radius},${latitude},${longitude});
-		);
-		out body;
-	`;
-
-	const response = await fetch('https://overpass-api.de/api/interpreter', {
-		method: 'POST',
-		body: query
-	});
-
-	if (!response.ok) {
-		throw new Error('Failed to fetch restaurants from Overpass API');
-	}
-
-	const data = (await response.json()) as OverpassResponse;
-	const restaurants: Restaurant[] = data.elements
-		.filter((el) => el.tags?.name)
-		.map((el) => ({
-			name: el.tags!.name!,
-			lat: el.lat || el.center?.lat || 0,
-			lon: el.lon || el.center?.lon || 0,
-			cuisine: el.tags!.cuisine
-		}))
-		.filter((r) => r.lat && r.lon);
-
-	return restaurants;
-}
+import { db, generateShortCode, getRestaurantsFromLocation } from '$lib';
 
 export const actions = {
 	// create: async ({ locals, request }) => {
